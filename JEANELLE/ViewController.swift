ee4f7c57bbee28cd12e4ab850ab6b60e7d1ef285
zhanel01 <zhumanovazhanel@mail.ru>
2025-12-17 01:31:07 +0500

    Initial Commit

---
+++ JEANELLE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JEANELLE
+//
+//  Created by Zhanel  on 17.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ JEANELLETests/JEANELLETests.swift
@@ -0,0 +1,17 @@
+//
+//  JEANELLETests.swift
+//  JEANELLETests
+//
+//  Created by Zhanel  on 17.12.2025.
+//
+
+import Testing
+@testable import JEANELLE
+
+struct JEANELLETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
